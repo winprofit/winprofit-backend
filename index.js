884987a694b3392e5e4f524cae020ae920aad894
Win Profit <winprofit2026@gmail.com>
2026-04-19 16:23:32 +0700

    fix cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 app.use('/api/entries',  require('./routes/entries'));
